08631fefa3f7e2e593f8970402cb11fe9e2e8087
Aayush Giri <aayushgiri1234@gmail.com>
2024-02-24 00:35:10 +0530

    sidebar-layout-style

---
+++ src/app/components/sidebar/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='fixed top-0 left-0 h-screen w-64 bg-white shadow-lg z-10'>Sidebar</div>
+  )
+}
+
+export default Sidebar
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Sidebar from "./components/sidebar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div className="flex h-screen w-full bg-gray-200">
+      <Sidebar/>
+      <div className="flex flex-col w-full h-full ml-64 p-4">{children}</div>
+      </div>
+      </body>
     </html>
   );
 }
